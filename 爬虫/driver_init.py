70f46a96cd92a9b995f3e6d726ae0befe2e253d1
m4rklee <59600279+m4rklee@users.noreply.github.com>
2023-05-16 15:25:02 +0800

    Update driver_init.py

--- 爬虫/driver_init.py
+++ 爬虫/driver_init.py
@@ -1,7 +1,15 @@
 from selenium import webdriver
+
 options = webdriver.ChromeOptions()
+
+# 开启无痕模式
 options.add_argument('--incognito')
+
+# 设置 headers 部分
 agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
 options.add_argument('user-agent=' + agent)
-options.add_experimental_option('detach', True)
+
+# 程序运行完毕后是否关闭浏览器，默认为关闭
+# options.add_experimental_option('detach', True)
+
 driver = webdriver.Chrome(options=options)
